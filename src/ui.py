54ea8fc54aaf1b5081566126a9482445dbd29bfd
foidkiller <ravilnatr@gmail.com>
2026-04-19 17:33:47 +0500

    Update ui.py

--- src/ui.py
+++ src/ui.py
@@ -2,35 +2,30 @@ from .models import PetState
 
 
 def get_pet_ascii(pet: PetState) -> str:
-    """Возвращает ASCII-артику питомца в зависимости от состояния"""
     health = pet.health
     mood = pet.mood
 
     if health <= 20 or mood >= 80:
-        # Плохое состояние
         return r"""
    /_/\  
   ( o.o ) 
    > ^ <  
-  (грустит...)
+  (sad...)
 """
     elif health >= 80 and mood <= 30:
-        # Отличное состояние
         return r"""
    /_/\  
   ( o.o ) 
    > ^ <  
-   (счастлив!)
+   (happy!)
 """
     elif health >= 50:
-        # Нормальное
         return r"""
    /_/\  
   ( o.o ) 
    > ^ <  
 """
     else:
-        # Среднее/устал
         return r"""
    /_/\  
   ( -.- ) 
